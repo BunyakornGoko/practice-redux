ed54aba51774b085ca7e09bbafe7e58c7315ade9
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-10-23 01:06:31 +0700

    func-bank-flow-redux

--- src/store.jsx
+++ src/store.jsx
@@ -9,17 +9,12 @@ const innitialState = {
 function reducer(state = innitialState, action) {
   switch (action.type) {
     case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload
-      }
+      return { ...state, balance: state.balance + action.payload }
     case "account/withdraw":
-      return {
-        ...state,
-        balance: state.balance - action.payload
-      }
+      return { ...state, balance: state.balance - action.payload }
     case "account/requestLoan":
       if (state.loan > 0) return state
+      // LATER
       return {
         ...state,
         loan: action.payload.amount,
@@ -40,16 +35,42 @@ function reducer(state = innitialState, action) {
 
 const store = createStore(reducer)
 
-store.dispatch({ type: "account/deposit", payload: 500 })
-store.dispatch({ type: "account/withdraw", payload: 200 })
+// store.dispatch({ type: "account/deposit", payload: 500 })
+// store.dispatch({ type: "account/withdraw", payload: 200 })
+// console.log(store.getState())
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" }
+// })
+// console.log(store.getState())
+
+// store.dispatch({ type: "account/payLoan" })
+
+// console.log(store.getState())
+// const ACCOUNT_DEPOSIT = "account/deposit"
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount }
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount }
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose }
+  }
+}
+function payLoan() {
+  return { type: "account/payLoan" }
+}
+
+store.dispatch(deposit(500))
+store.dispatch(withdraw(200))
 console.log(store.getState())
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" }
-})
+store.dispatch(requestLoan(1000, "Buy a cheap car"))
 console.log(store.getState())
-
-store.dispatch({ type: "account/payLoan" })
-
+store.dispatch(payLoan())
 console.log(store.getState())
