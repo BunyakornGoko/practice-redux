8bb49cda6eb7d9d7fc60e807036fce9838497c99
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-10-23 11:34:48 +0700

    redux-slice-store

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
-import "./store.jsx"
+
+import store from "./store.jsx"
+
+store.dispatch({ type: "account/deposit", payload: 250 })
+console.log(store.getState())
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
